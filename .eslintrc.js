5506c1df92471ff67f02393388cc6b778056bbbc
Takamasa Oshikiri <mail@oshikiri.org>
2018-11-17 17:17:22 +0900

    Add eslint-plugin-chai-friendly

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,13 @@ module.exports = {
         "strict": 0,
         "comma-dangle": 0,
         "prefer-destructuring": 0
-    }
+    },
+    plugins: ['chai-friendly'],
+    overrides: [{
+      files: 'test/*.js',
+      rules: {
+        'no-unused-expressions': 'off',
+        'chai-friendly/no-unused-expressions': 'off',
+      },
+    }]
 }

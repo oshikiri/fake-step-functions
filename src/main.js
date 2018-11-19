753f9bebc087734e991189f2a7a3e35af74bba85
Takamasa Oshikiri <mail@oshikiri.org>
2018-11-19 22:16:44 +0900

    Add ./src/main.js

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const FakeStateMachine = require('./FakeStateMachine').FakeStateMachine;
+const RunStateResult = require('./RunStateResult').RunStateResult;
+
+module.exports = {
+  FakeStateMachine,
+  RunStateResult,
+};
